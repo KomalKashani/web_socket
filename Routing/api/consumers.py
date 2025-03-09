4ef22c0798f6b8d0d82704b1d331c941be9fe9b4
komal <komalkashani2002@gmail.com>
2025-03-09 23:49:35 +0530

    added routing notes

--- Routing/api/consumers.py
+++ Routing/api/consumers.py
@@ -1,32 +1,37 @@
 from channels.consumer import SyncConsumer,AsyncConsumer
-
-
-"""SYNC CONSUMER"""
+from channels.exceptions import StopConsumer
 
 class MySyncConsumer(SyncConsumer):
     
     # This handler is called when client initially opens a 
     # connection and is about to finish the websocket handhshake.
     def websocket_connect(self,event):
-        print("Websocket Connected...")
+        print("Websocket Connected...",event)
+        self.send({
+            'type':'websocket.accept'
+        })
 
     # This handler is called when data received from client
     def websocket_receive(self,event):
-        print("Messaged Received....")
+        print("Messaged Received....",event)
+        print("Messaged is....",event['text'])
+
 
     # This handler is called when either connection to the client is lost, either from the 
     # client closing the connection, the server closing the connection, or loss of the socket.
     def websocket_disconnect(self,event):
         print("Websocket Disconnected...")
-
-"""ASYNC CONSUMER"""
+        raise StopConsumer()
 
 class MyAsyncConsumer(AsyncConsumer):
     
     # This handler is called when client initially opens a 
     # connection and is about to finish the websocket handhshake.
     async def websocket_connect(self,event):
-        print("Websocket Connected...")
+        print("Websocket Connected...",event)
+        await self.send({
+            'type':'websocket.accept'
+        })
 
     # This handler is called when data received from client
     async def websocket_receive(self,event):
@@ -36,3 +41,4 @@ class MyAsyncConsumer(AsyncConsumer):
     # client closing the connection, the server closing the connection, or loss of the socket.
     async def websocket_disconnect(self,event):
         print("Websocket Disconnected...")
+        raise StopConsumer()
